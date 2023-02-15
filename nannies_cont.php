8c0afc0d1fa73edb2e381df394f45601042add98
ATL-codes <83129923+ATL-codes@users.noreply.github.com>
2023-02-15 20:04:36 +0000

    Add files via upload

---
+++ nannies_cont.php
@@ -0,0 +1,30 @@
+<!-- Page Header Start here -->
+  <section class="page-header section-notch">
+    <div class="overlay">
+      <div class="container">
+        <h3>Meet Our Sitters</h3>
+        <ul>
+          <li><a href="home.php">Home</a></li>
+          <li>-</li>
+          <li>Sitters</li>
+        </ul>
+      </div><!-- container -->
+    </div><!-- overlay -->
+  </section><!-- page header -->
+  <!-- Page Header End here -->
+
+
+ 
+  <!-- <!-- <!-- Teachers Start here -->
+  <section class="teachers teachers-page padding-120">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-xs-12">
+          <div class="teacher-item">
+            <h1>Coming Soon!!</h1>
+          </div><!-- teacher item -->
+        </div>
+      </div><!-- row -->
+    </div><!-- container -->
+  </section><!-- teacher -->
+  <!-- Teachers End here -->
